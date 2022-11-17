c776a57b56917164f576a699630d9a9d48b88113
brian <brian.cuppen@student.howest.be>
2022-11-17 18:18:41 +0100

    commit

---
+++ script/app.js
@@ -0,0 +1,11 @@
+//Animeert de robot 50 eenheden naar rechts vanaf de startpositie
+    //gsap.to('#Robot', { duration: 1, x: 50 });
+
+//Plaatst de robot eerst 50 eenheden naar rechts en animeert de robot naar de startpositie
+    //gsap.from('#Robot', { duration: 1, x: 50 });
+
+//Plaatst de robot 50 eenheden naar rechts zonder animatie
+    //gsap.set('#Robot', { duration: 1, x: 50 });
+    
+//Animeert de robot van een positie -50 links naar rechts 50.
+    //gsap.fromTo('#Robot', { x: -50 }, { duration: 1, x: 50 })
